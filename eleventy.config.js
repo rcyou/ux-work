511952ed6781d29abba8f0493cd385c74f067cae
Ryan Young <ryoung.hello@gmail.com>
2026-01-18 08:50:50 -0600

    Styling my RSS feed

--- eleventy.config.js
+++ eleventy.config.js
@@ -17,6 +17,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/robots.txt");
   eleventyConfig.addPassthroughCopy("src/BingSiteAuth.xml");
   eleventyConfig.addPassthroughCopy("src/googlec6dede463ff3417e.html")
+  eleventyConfig.addPassthroughCopy("src/pretty-feed-v3.xsl")
 
   eleventyConfig.addFilter("utcDate", function(value, arg) { 
     return moment(value).utc().format(arg);
